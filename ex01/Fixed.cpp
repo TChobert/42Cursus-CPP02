e908f9a53297e6722214d477da6663edb616fc1f
Thomas Chobert <tchobert@f4r1s2.clusters.42paris.fr>
2025-05-14 17:12:28 +0200

    ex01 - int constructor

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 19:20:59 by tchobert          #+#    #+#             */
+/*   Updated: 2025/05/13 19:21:12 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int	Fixed::fractionalBitsNb = 8;
+
+Fixed::Fixed(void) {
+	_numberValue = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int intValue) {
+	std::cout << "Int constructor called" << std::endl;
+	_numberValue = (intValue * (1 << fractionalBitsNb));
+}
+
+Fixed::Fixed(const Fixed &obj) {
+	_numberValue = obj._numberValue;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignement operator called" << std::endl;
+	if (this != &other) {
+		this->_numberValue = other._numberValue;
+	}
+	return (*this);
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_numberValue);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	_numberValue = raw;
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 16:02:51 by tchobert          #+#    #+#             */
+/*   Updated: 2025/05/14 16:03:03 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (EXIT_SUCCESS);
+}
